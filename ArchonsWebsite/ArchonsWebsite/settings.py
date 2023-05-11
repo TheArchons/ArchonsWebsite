8a22c5bd4666060a7c71838fe3cfcae4c177099b
TheArchons <progamer9138@gmail.com>
2023-05-11 10:17:11 -0700

    load settings from .env

--- ArchonsWebsite/ArchonsWebsite/settings.py
+++ ArchonsWebsite/ArchonsWebsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from json import load
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,13 +20,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
+# load .env
+
+env = load(open(".env"))
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^gn4)e9zw+u8$m*mh5!&gxll)ae+mn4%np3bs-lh^swm@2#+5=' # Fill in with your own secret key
+SECRET_KEY = env["SECRET_KEY"]  # Fill in with your own secret key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = env["DEBUG"]
 
-ALLOWED_HOSTS = ['.thearchons.xyz', '35.86.14.35', "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = env["ALLOWED_HOSTS"]
 
 
 # Application definition
