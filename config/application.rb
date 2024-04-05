4c2a91e548ae02901bcd8c9b8189196235b24740
TheArchons <progamer9138@gmail.com>
2024-04-04 18:47:08 -0700

    add dev.thearchons.xyz to the list of allowed hosts

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module ArchonsWebsite
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # add dev.thearchons.xyz to the list of allowed hosts
+    config.hosts << "dev.thearchons.xyz"
   end
 end
